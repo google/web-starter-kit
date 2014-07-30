ddbe84e87316884bb0278eed025b90a170212723
mattcooper <matt@mcooper.co.uk>
2014-07-30 14:39:00 +0100

    =Updated to move some helper functions out into util.js

--- material/lite/input.js
+++ material/lite/input.js
@@ -1,9 +1,5 @@
 (function(){
   window.onload = function(){
-    var hasClass = function(el, cl){return el.className.match(new RegExp('(\\s|^)'+ cl +'(\\s|$)'))}
-    var addClass = function(el, cl){if (!hasClass(el,cl)) el.className += " "+cl;}
-    var removeClass = function(el, cl){if (hasClass(el,cl)){el.className=el.className.replace(new RegExp('(\\s|^)'+cl+'(\\s|$)'),' ');}}
-    
     var inputs = document.getElementsByClassName('text-input');
 
     for(var i = 0; i < inputs.length; i++){
