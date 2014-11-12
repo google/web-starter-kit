c8996cbcf27baecae6daf3ae18c4080b59f1a0a7
Sérgio Gomes <sgomes@google.com>
2014-11-12 16:21:45 +0000

    Adding button demo for review by UX folks.

---
+++ button-review/styleguide/third_party/rAF.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// From: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
