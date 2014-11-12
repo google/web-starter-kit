ffdcdea7224cda9a1c81d1fd4035badf17ddd840
Addy Osmani <addyosmani@gmail.com>
2014-11-12 17:17:00 +0000

    Remove old Material preview. So so old

--- material/lite/utils.js
+++
@@ -1,3 +0,0 @@
-var hasClass = function(el, cl){return el.className.match(new RegExp('(\\s|^)'+ cl +'(\\s|$)'))}
-var addClass = function(el, cl){if (!hasClass(el,cl)) el.className += " "+cl;}
-var removeClass = function(el, cl){if (hasClass(el,cl)){el.className=el.className.replace(new RegExp('(\\s|^)'+cl+'(\\s|$)'),' ');}}
