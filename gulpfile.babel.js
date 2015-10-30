54bbf66a70ec9b7ff863d6e13390fdd93c4195dd
Jeff Posnick <jeffy@google.com>
2015-10-30 15:53:41 -0400

    Implements runtime SW caching for some CDN resources (Closes #771)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -223,18 +223,29 @@ gulp.task('pagespeed', cb =>
   }, cb)
 );
 
+// Copy over the scripts that are used in importScripts as part of the generate-service-worker task.
+gulp.task('copy-sw-scripts', () => {
+  return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
+    .pipe(gulp.dest('dist/scripts/sw'));
+});
+
 // See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
 // an in-depth explanation of what service workers are and why you should care.
 // Generate a service worker file that will provide offline functionality for
 // local resources. This should only be done for the 'dist' directory, to allow
 // live reload to work as expected when serving from the 'app' directory.
-gulp.task('generate-service-worker', () => {
+gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
   const rootDir = 'dist';
   const filepath = path.join(rootDir, 'service-worker.js');
 
   return swPrecache.write(filepath, {
     // Used to avoid cache conflicts when serving on localhost.
     cacheId: pkg.name || 'web-starter-kit',
+    // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
+    importScripts: [
+      'scripts/sw/sw-toolbox.js',
+      'scripts/sw/runtime-caching.js'
+    ],
     staticFileGlobs: [
       // Add/remove glob patterns to match your directory setup.
       `${rootDir}/images/**/*`,
