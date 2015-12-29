75b553b4b06bae1de5c660b5066bea10900bda85
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2015-12-28 16:28:44 -0800

    Remove unused callback in del task
    I believe del is returning a stream or promise here so we don't need (and never
    use) the callback. If we do need the callback let's edit this commit to use it.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -151,7 +151,7 @@ gulp.task('html', () => {
 });
 
 // Clean output directory
-gulp.task('clean', cb => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
 // Watch files for changes & reload
 gulp.task('serve', ['scripts', 'styles'], () => {
