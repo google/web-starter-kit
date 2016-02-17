87a65c9c07a4ffd1d3a1280aa259c39a0776bacd
Matthew  Gaunt <matt@gauntface.co.uk>
2016-02-17 17:31:05 +0000

    This needs a refactor and tidy up

--- test/task-watch-test.js
+++ test/task-watch-test.js
@@ -38,6 +38,7 @@ const TEST_OUTPUT_PATH = path.join('test', 'output');
 const TEST_OUTPUT_SRC = path.join(TEST_OUTPUT_PATH, 'src');
 const TEST_OUTPUT_DEST = path.join(TEST_OUTPUT_PATH, 'build');
 
+let watcherTaskId = 0;
 let watcherTask;
 
 const copyFiles = (from, to) => {
@@ -54,12 +55,14 @@ const copyFiles = (from, to) => {
 };
 
 const validateOutput = () => {
+  // console.log('validateOutput');
   // Get directories in build directory
   const folders = fs.readdirSync(TEST_OUTPUT_DEST);
   folders.forEach(folderName => {
     try {
       // Check if the source directory lives, if it doesn't we can ignore
       // the remaining output (i.e. old stuff that needs cleaning out)
+      // console.log('Running lstat');
       fs.lstatSync(path.join(TEST_OUTPUT_SRC, folderName));
     } catch (err) {
       // Path doesn't exist, we can ignore it
@@ -70,13 +73,13 @@ const validateOutput = () => {
     const expectedOutput = JSON.parse(expectedOutputFileBuffer.toString());
     expectedOutput.forEach(file => {
       const fullpath = path.join(TEST_OUTPUT_DEST, folderName, file);
+      console.log('lstat');
       const pathstats = fs.lstatSync(fullpath);
       if (!pathstats) {
         throw new Error(`Expected output file could not be found: ${fullpath}`);
       }
 
       if (pathstats.size <= 0) {
-        console.log(pathstats);
         throw new Error(`Output file has no contents: ${fullpath}`);
       }
     });
@@ -90,37 +93,55 @@ const runSteps = (taskName, task, steps) => {
     let currentTimeout = null;
 
     // Start Watching
+    watcherTaskId++;
     watcherTask = task.watch();
     if (!watcherTask) {
       reject(new Error(`Nothing returned from the tasks watch() method. Is the result of gulp.watch returned in ${taskName}`));
+      return;
     }
+
+    watcherTask._customId = watcherTaskId;
 
     // Listen to events to detect when changes are handled and add a short delay
     // to give task time to complete
-    watcherTask.on('all', () => {
+    watcherTask.on('all', (event) => {
+      console.log('Watch Event: ', event, watcherTask._customId);
       if (currentTimeout) {
         clearTimeout(currentTimeout);
       }
 
+      console.log('Watch Event: Step 1', event, watcherTask._customId);
+
       currentTimeout = setTimeout(() => {
+        console.log('Watch Event: Step 3 (Timeout)', event, watcherTask._customId);
         if (stepIndex === (steps.length - 1)) {
+          console.log('Steps Finished - Closing Watcher ' + watcherTask._customId);
           resolve();
           return;
         }
 
         stepIndex++;
         steps[stepIndex]();
-      }, 1000);
+      }, 6000);
+
+      console.log('Watch Event: Step 2', event, watcherTask._customId);
     });
 
     // Listen for when to start changes to files
     watcherTask.on('ready', steps[stepIndex]);
   })
+  .then(function() {
+    console.log('Promise Has Finished');
+    if (watcherTask) {
+      watcherTask.close();
+      watcherTask = null;
+    }
+  })
   .then(validateOutput);
 };
 
 const registerTestsForTask = (taskName, task) => {
-  describe(`Test the watch task of ${taskName}`, function() {
+  describe(`${taskName}`, function() {
     it('should watch for new files being added to empty directory', function() {
       // This is a long time to account for slow babel builds on Windows
       this.timeout(60000);
@@ -153,7 +174,11 @@ const registerTestsForTask = (taskName, task) => {
         () => del(TEST_OUTPUT_SRC + '/*')
       ];
 
-      return runSteps(taskName, task, steps);
+      return runSteps(taskName, task, steps)
+        .catch(err => {
+          console.log('Error from Task', err);
+          throw err;
+        });
     });
 
     it('should watch for new files being added, followed by bad example files followed by the original files', function() {
@@ -187,12 +212,19 @@ const registerTestsForTask = (taskName, task) => {
 describe('Run tests against watch methods', function() {
   // Clean up before each test
   beforeEach(done => {
-    rimraf(path.join(TEST_OUTPUT_PATH, '**'), () => {
-      if (watcherTask) {
-        watcherTask.close();
-        watcherTask = null;
-      }
+    console.log('beforeEach Step 1');
+    if (watcherTask) {
+      console.log('Watcher task .close()');
+      watcherTask.close();
+      watcherTask = null;
+    }
+    console.log('beforeEach Step 2');
 
+    // Use rimraf over del because it seems to work more reliably on Windows.
+    // Probably due to it's retries.
+    rimraf(path.join(TEST_OUTPUT_PATH, '**'), function() {
+      // Create Source Path
+      // console.log('beforeEach Step 3');
       mkdirp.sync(TEST_OUTPUT_SRC);
 
       GLOBAL.config = {
@@ -203,38 +235,18 @@ describe('Run tests against watch methods', function() {
 
       done();
     });
-    console.log('Going to attempt to delete: ', path.join(TEST_OUTPUT_PATH, '**'));
-    // return del(path.join(TEST_OUTPUT_PATH, '**'), {dot: true})
-    // .catch(err => {
-    //   console.log(err);
-    //   throw err;
-    // })
-    /** .then(() => {
-      if (watcherTask) {
-        watcherTask.close();
-        watcherTask = null;
-      }
-    })
-    .then(() => {
-      // Create Source Path
-      mkdirp.sync(TEST_OUTPUT_SRC);
-
-      GLOBAL.config = {
-        env: 'dev',
-        src: TEST_OUTPUT_SRC,
-        dest: TEST_OUTPUT_DEST
-      };
-    });**/
   });
 
   // Clean up after final test
   after(done => {
+    if (watcherTask) {
+      watcherTask.close();
+      watcherTask = null;
+    }
+
+    // Use rimraf over del because it seems to work more reliably on Windows.
+    // Probably due to it's retries.
     rimraf(path.join(TEST_OUTPUT_PATH, '**'), done);
-    // return del(path.join(TEST_OUTPUT_PATH, '**'), {dot: true}).catch(() => {})
-    // .catch(err => {
-    //   console.log(err);
-    //   throw err;
-    // });
   });
 
   taskHelper.getTasks().map(taskObject => {
