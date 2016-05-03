5374936f50a71cc7096558d4829c10d24a9bb0d9
Nicolas Hoizey <nicolas@hoizey.com>
2016-05-03 17:06:00 +0200

    Consistency with every other paths

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -109,7 +109,7 @@ gulp.task('scripts', () =>
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
-      './app/scripts/main.js'
+      'app/scripts/main.js'
       // Other scripts
     ])
       .pipe($.newer('.tmp/scripts'))
