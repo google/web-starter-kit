a0bf7da662fe58178f697e0de433be3aab41bd0e
Kamil Wojtczyk <k.wojtczyk@gmail.com>
2016-09-09 20:58:26 +0100

    Added new functionality to gulp
    Gulp will now scan default @@tags in html files and replace those with
    content of included files
    
    To follow the live-reloading functionality, html files are now watched
    and compiled to the .tmp folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -127,11 +127,16 @@ gulp.task('scripts', () =>
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
-  return gulp.src('app/**/*.html')
+  return gulp.src(['app/**/*.html', '!app/partials/*.html'])
+    .pipe($.fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
     .pipe($.useref({
       searchPath: '{.tmp,app}',
       noAssets: true
     }))
+    .pipe(gulp.dest('.tmp/'))
 
     // Minify any HTML
     .pipe($.if('*.html', $.htmlmin({
@@ -154,7 +159,7 @@ gulp.task('html', () => {
 gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
 
 // Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
+gulp.task('serve', ['scripts', 'styles', 'html'], () => {
   browserSync({
     notify: false,
     // Customize the Browsersync console logging prefix
@@ -169,7 +174,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
     port: 3000
   });
 
-  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/**/*.html'], ['html', reload]);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
   gulp.watch(['app/images/**/*'], reload);
