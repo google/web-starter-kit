a353d9078f4a9d77202e2b7ff147891d4bdbcf15
Matt Gaunt <matt@gauntface.co.uk>
2018-03-02 07:36:58 -0800

    Fixing build loop with sw task

--- gulp-tasks/serviceWorker.js
+++ gulp-tasks/serviceWorker.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const workboxBuild = require('workbox-build');
 
+const SW_FILENAME = 'service-worker.js';
+
 async function serviceWorker() {
   await workboxBuild.injectManifest({
-    swSrc: path.join(global.__buildConfig.src, 'service-worker.js'),
-    swDest: path.join(global.__buildConfig.dest, 'service-worker.js'),
+    swSrc: path.join(global.__buildConfig.src, SW_FILENAME),
+    swDest: path.join(global.__buildConfig.dest, SW_FILENAME),
     globDirectory: global.__buildConfig.dest,
   });
 }
 
 module.exports = {
-  // build: serviceWorker,
-  watchGlobs: `${global.__buildConfig.dest}/**/*.*`,
+  build: serviceWorker,
+  watchGlobs: [
+    `${global.__buildConfig.dest}/**/*.*`,
+    `!${global.__buildConfig.dest}/${SW_FILENAME}`
+  ],
 };
